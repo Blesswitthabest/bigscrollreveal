adec66e7b8bcde65fa8eba968cd09a8d434ca391
Blesswitthabest <wils2140@msu.edu>
2026-02-21 18:28:53 -0600

    Add ScrollReveal animations for photo elements

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+ScrollReveal({
+  reset: false,
+  distance: '80px',
+  duration: 1000,
+  easing: 'ease-in-out'
+});
+
+ScrollReveal().reveal('.photo1', { origin: 'left', delay: 200 });
+ScrollReveal().reveal('.photo2', { origin: 'right', delay: 300 });
+ScrollReveal().reveal('.photo3', { origin: 'bottom', delay: 400 });
+ScrollReveal().reveal('.photo4', { scale: 0.7, delay: 500 });
+ScrollReveal().reveal('.photo5', { rotate: { x: 20, z: 10 }, delay: 600 });
 
