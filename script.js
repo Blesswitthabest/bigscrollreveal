d2c7fc5eb2363fd9acf55ec5ac2db383a855f1d9
Blesswitthabest <wils2140@msu.edu>
2026-02-21 18:42:03 -0600

    Adjust reveal delays for photo animations

--- script.js
+++ script.js
@@ -5,9 +5,9 @@ ScrollReveal({
   easing: 'ease-in-out'
 });
 
-ScrollReveal().reveal('.photo1', { origin: 'left', delay: 400 });
-ScrollReveal().reveal('.photo2', { origin: 'right', delay: 800 });
-ScrollReveal().reveal('.photo3', { origin: 'bottom', delay: 1200 });
+ScrollReveal().reveal('.photo1', { origin: 'left', delay: 100 });
+ScrollReveal().reveal('.photo2', { origin: 'right', delay: 400 });
+ScrollReveal().reveal('.photo3', { origin: 'bottom', delay: 800 });
 ScrollReveal().reveal('.photo4', { scale: 0.7, delay: 1500 });
 ScrollReveal().reveal('.photo5', { rotate: { x: 20, z: 10 }, delay: 2000 });
 
